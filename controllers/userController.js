32c510b3a07c8cb7983bac9491b85e05938d251d
soodala2003 <soodala78@gmail.com>
2025-05-02 05:56:47 -0400

    fix update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,8 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import Job from '../models/JobModel.js';
 import cloudinary from 'cloudinary';
-import { promises as fs } from 'fs'; //file system module
+import { formatImage } from '../middleware/multerMiddleware.js';
+//import { promises as fs } from 'fs'; //file system module
 
 // to get the info about the actual user,
 // username:  running every time user navigates to the dashboard.
@@ -26,9 +27,13 @@ export const updateUser = async (req, res) => {
   delete newUser.password;
 
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
+    const file = formatImage(req.file);
+    //return;
+    const response = await cloudinary.v2.uploader.upload(file);
+    //const response = await cloudinary.v2.uploader.upload(req.file.path);
     //it returns an object
-    await fs.unlink(req.file.path);
+
+    //await fs.unlink(req.file.path);
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,7 +1,11 @@
 import multer from 'multer';
+import DataParser from 'datauri/parser.js';
+import path from 'path';
 
-const storage = multer.diskStorage({
-  /* cb <= callback */
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+/* const storage = multer.diskStorage({
+  //cb <= callback 
   destination: (req, file, cb) => {
     // 'public/uploads': the actual location, the destination <= to set the directory where uploaded files will be stored
     cb(null, 'public/uploads');
@@ -12,7 +16,16 @@ const storage = multer.diskStorage({
     // set the name of the uploaded file
     cb(null, fileName);
   },
-});
-const upload = multer({ storage });
+}); */
+
+const parser = new DataParser();
+
+// when we run Malter, essentially
+// we add this file to a request object
+export const formatImage = (file) => {
+  //console.log(file);
+  const fileExtension = path.extname(file.originalname).toString();
+  return parser.format(fileExtension, file.buffer).content;
+};
 
 export default upload;
